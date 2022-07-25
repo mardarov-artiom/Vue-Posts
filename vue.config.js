99c4e69ce088b793aa4f9bb04e5d6bcd15b41cff
Artiom Mardarov <mardarov.artiom@gmail.com>
2022-07-25 11:14:49 +0300

    updated vue.config ( test )

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,4 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: 'build',
-  assetsDir: 'static/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/Vue-Posts/' : '/',
 });
